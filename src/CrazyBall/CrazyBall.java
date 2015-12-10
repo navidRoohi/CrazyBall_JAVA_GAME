f28b1825e2942d929c033c30c38ad26294ad608c
Navid Roohi <navidRoohi@users.noreply.github.com>
2015-12-10 15:14:21 -0800

    Update CrazyBall.java

--- src/CrazyBall/CrazyBall.java
+++ src/CrazyBall/CrazyBall.java
@@ -17,7 +17,6 @@ import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 /**
  *
  * @author navidroohibroojeni
@@ -37,48 +36,40 @@ public class CrazyBall extends Applet implements Runnable, KeyListener {
     Award item[] = new Award[10];
     Enemy enemy[] = new Enemy[5];
     Rain rain[] = new Rain[100];
-   
-    
+
     public static int score;
-    
+
     double cityX = 0;
     double ciyuY = 0;
     double cityDx = 3;
     Image backGroundImage, enemyImage, iconScoreImage;
-    
+
     Image imageListBack[] = new Image[2];
-    
+
     int countImage = 0;
-    
-   
-        
+
     public static boolean gameOver = false;
-    
-    
-    public static AudioClip birdAudioClip, backgroundAudioClip, 
-            nightTimeAudioClip, thunderAudioClip, 
+
+    public static AudioClip birdAudioClip, backgroundAudioClip,
+            nightTimeAudioClip, thunderAudioClip,
             ballAudioClip, coinAudioClip;
-    
 
     // init get call then will call start method
     @Override
     public void init() {
         setSize(800, 600);
         addKeyListener(this);
-        
 
-        
         // background 
         imageListBack[0] = getImage(getCodeBase(), "MediaFiles/b1.png");
         imageListBack[1] = getImage(getCodeBase(), "MediaFiles/b2.png");
 
-
         enemyImage = getImage(getCodeBase(), "MediaFiles/enemy.gif");
         iconScoreImage = getImage(getCodeBase(), "MediaFiles/imageSmall.gif");
-        
+
         birdAudioClip = getAudioClip(getCodeBase(), "MediaFiles/bird_caw1.wav");  // bird sounds, when they collide 
         backgroundAudioClip = getAudioClip(getCodeBase(), "MediaFiles/candy.wav");  // background music 
-        nightTimeAudioClip = getAudioClip(getCodeBase(), "MediaFiles/night_time.wav"); 
+        nightTimeAudioClip = getAudioClip(getCodeBase(), "MediaFiles/night_time.wav");
         thunderAudioClip = getAudioClip(getCodeBase(), "MediaFiles/thunder.wav");
         ballAudioClip = getAudioClip(getCodeBase(), "MediaFiles/ball.wav");  // ball hit the platforms sound
         coinAudioClip = getAudioClip(getCodeBase(), "MediaFiles/coin.wav");    // award sounds, when ball catch other balls
@@ -87,14 +78,13 @@ public class CrazyBall extends Applet implements Runnable, KeyListener {
          MediaTracker tr = new MediaTracker(this);
          tr.addImage(img, 0);    // add the image to media tracker
          */
-        
+
     }
 
     // start method start after init method
     // 
     @Override
     public void start() {
-        
 
         b = new Jumper();
         score = 0;
@@ -111,49 +101,43 @@ public class CrazyBall extends Applet implements Runnable, KeyListener {
         for (int i = 0; i < rain.length; i++) {
             rain[i] = new Rain(getHeight() + 200 * i);
         }
-        
-        
-         
-        
+
         Thread thread = new Thread(this);
         thread.start();
-        
+
     }
-    
+
     @Override
     public void run() {
-               
-        
-        
-                backgroundAudioClip.loop();
-                nightTimeAudioClip.loop();
-                thunderAudioClip.loop();
 
-         while (true) {
-             
-             if (gameOver){
-                 b.setDt(0);
-                 b.setX(-30);
-                 b.setY(-30);
-                
-             }
-             
+        backgroundAudioClip.loop();
+        nightTimeAudioClip.loop();
+        thunderAudioClip.loop();
 
-             if (cityX > getWidth() * -1 ){
-                 cityX -= cityDx;
-             }else{
-                 cityX=0;
-             }
-        
-        for ( int i=0 ; i <item.length; i++){
-                if (item[i].getY() == this.getHeight() + 100){
+        while (true) {
+
+            if (gameOver) {
+                b.setDt(0);
+                b.setX(-30);
+                b.setY(-30);
+
+            }
+
+            if (cityX > getWidth() * -1) {
+                cityX -= cityDx;
+            } else {
+                cityX = 0;
+            }
+
+            for (int i = 0; i < item.length; i++) {
+                if (item[i].getY() == this.getHeight() + 100) {
                     item[i] = null;
                     item[i] = new Award(getWidth());
                 }
             }
- 
+
             b.update(this);
-            
+
             for (int i = 0; i < floor.length; i++) {
                 floor[i].update(this, b);
             }
@@ -164,24 +148,24 @@ public class CrazyBall extends Applet implements Runnable, KeyListener {
                 enemy[i].update(this, b);
             }
             for (int i = 0; i < rain.length; i++) {
-                 rain[i].update(this);
-        }
-            
+                rain[i].update(this);
+            }
+
             repaint();
             try {
                 Thread.sleep(17);  // millisecond
             } catch (InterruptedException ex) {
                 Logger.getLogger(CrazyBall.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
         }
     }
-    
+
     @Override
     public void stop() {
         super.stop(); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public void destroy() {
         super.destroy(); //To change body of generated methods, choose Tools | Templates.
@@ -200,60 +184,68 @@ public class CrazyBall extends Applet implements Runnable, KeyListener {
         }
         doubleG.setColor(getBackground());  // same back ground color
         doubleG.fillRect(0, 0, this.getSize().width, this.getSize().height);
-        
+
         doubleG.setColor(getForeground());
         paint(doubleG);
-        
+
         g.drawImage(i, 0, 0, this);
     }
-    
+
     @Override
     public void paint(Graphics g) {
-        
-       //  g.setColor(new Color(15,77,147));
-                 g.setColor(Color.BLACK);
 
-        g.fillRect(0, 0, getWidth()+2400, getHeight());
-        g.drawImage(imageListBack[0],(int)cityX,0, this);
-        g.drawImage(imageListBack[1],(int)cityX+ getWidth(),0, this);
-        
-        
-       // g.drawImage(backGroundImage,(int)cityX + getWidth(),0, this);
-        
+        //  g.setColor(new Color(15,77,147));
+        g.setColor(Color.BLACK);
+
+        g.fillRect(0, 0, getWidth() + 2400, getHeight());
+        g.drawImage(imageListBack[0], (int) cityX, 0, this);
+        g.drawImage(imageListBack[1], (int) cityX + getWidth(), 0, this);
+
+        // g.drawImage(backGroundImage,(int)cityX + getWidth(),0, this);
         b.paint(g);
-         for (int i = 0; i < floor.length; i++) {
-                floor[i].paint(g);
-            }
-         for (int i = 0; i < item.length; i++) {
-                item[i].paint(g);
-            }
-         for (int i = 0; i < enemy.length; i++) {
-                enemy[i].paint(g);
-            }
-         for (int i = 0; i < rain.length; i++) {
-                rain[i].paint(g);
+        for (int i = 0; i < floor.length; i++) {
+            floor[i].paint(g);
         }
-         
-         String scoretext = "Score: " + Integer.toString(score);
-         Font font = new Font("Serif", Font.BOLD, 32);
-         g.setFont(font);
-         g.setColor(Color.BLACK);
-         g.drawString(scoretext, 70+2, 70+2);  // ceate shadow for the score text
-         g.setColor(new Color(198,255,255));
-         g.drawString(scoretext, 70, 70);
-         g.drawImage(iconScoreImage, 20, 20, this);  // icon next to the score text 
-       
+        for (int i = 0; i < item.length; i++) {
+            item[i].paint(g);
+        }
+        for (int i = 0; i < enemy.length; i++) {
+            enemy[i].paint(g);
+        }
+        for (int i = 0; i < rain.length; i++) {
+            rain[i].paint(g);
+        }
+
+        String scoretext = "Score: " + Integer.toString(score);
+        Font font = new Font("Serif", Font.BOLD, 32);
+        g.setFont(font);
+        g.setColor(Color.BLACK);
+        g.drawString(scoretext, 70 + 2, 70 + 2);  // ceate shadow for the score text
+        g.setColor(new Color(198, 255, 255));
+        g.drawString(scoretext, 70, 70);
+        g.drawImage(iconScoreImage, 20, 20, this);  // icon next to the score text 
+        Font font3 = new Font("Serif", Font.BOLD, 32);
+        g.setFont(font3);
+
+        
+        // I need to add the user if he want to continiu the game
+        
+        String gameOverString = "GAME OVER";
+        if (CrazyBall.gameOver) {
+            g.drawString(gameOverString, getWidth() / 2 - 75, getHeight() / 2);
+
+        }
 
     }
-    
+
     @Override
     public void keyTyped(KeyEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public void keyPressed(KeyEvent e) {
-        
+
         switch (e.getKeyCode()) {
             case KeyEvent.VK_LEFT:
                 b.moveLeft();
@@ -261,10 +253,10 @@ public class CrazyBall extends Applet implements Runnable, KeyListener {
             case KeyEvent.VK_RIGHT:
                 b.moveRight();
                 break;
-            
+
         }
     }
-    
+
     @Override
     public void keyReleased(KeyEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
